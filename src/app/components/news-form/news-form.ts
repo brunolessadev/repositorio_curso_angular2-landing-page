8aa23f04e47c1affe855dde23cd32f103a071b3c
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-15 01:19:54 -0300

    landing-page_1.5--finalizado-backend-formulario

--- src/app/components/news-form/news-form.ts
+++ src/app/components/news-form/news-form.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BtnPrimary } from "../btn-primary/btn-primary";
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Newsletter } from '../../services/newsletter';
 
 @Component({
   selector: 'news-form',
@@ -8,9 +9,34 @@ import { ReactiveFormsModule } from '@angular/forms';
     BtnPrimary,
     ReactiveFormsModule
    ],
+   providers: [
+    Newsletter
+   ],
   templateUrl: './news-form.html',
   styleUrl: './news-form.scss',
 })
 export class NewsForm {
 
+  newsletterForms!: FormGroup;
+  loading = signal (false);
+  
+
+  constructor(private service: Newsletter){
+    this.newsletterForms = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  onSubmit(){
+    this,this.loading.set(true);
+    if(this.newsletterForms.valid){
+      this.service.sendData(this.newsletterForms.value.name, this.newsletterForms.value.email).subscribe({
+        next: () => {
+          this.newsletterForms.reset();
+          this,this.loading.set(false);
+        }
+      })
+    }
+  }
 }
